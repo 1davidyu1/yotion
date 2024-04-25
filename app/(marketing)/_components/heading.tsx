012bfaae96af5ebb5e966b8d8e6bebb117a1cd74
1davidyu1 <yudavid@msu.edu>
2024-04-25 11:55:44 -0400

    landing page & dark mode

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,11 +1,21 @@
 "use client"
+import { ArrowRight } from "lucide-react"
+import { Button } from "@/components/ui/button"
 
 export const Heading = () => {
     return (
         <div className="max-w-3xl space-x-4">
-            <h1>
-                Your Ideas, Documents & Plans. Unified. Welcome to Yotion
+            <h1 className="text-3xl sm:text-5xl md:text-6xl">
+                Your Ideas, Documents & Plans. Unified. Welcome to Yotion <span className="underline">Yotion</span>
             </h1>
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+                Yotion is the connected workspace where <br />
+                better, faster work happens
+            </h3>
+            <Button>
+                Enter Yotion
+                <ArrowRight className="h-4 w-4 ml-2"/>
+            </Button>
         </div>
     )
 }
