2bcd69b1e412662e4dfb1c58c4c10a48d72b1b4d
1davidyu1 <yudavid@msu.edu>
2024-04-24 22:25:28 -0400

    basic heading and favicon

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export const Heading = () => {
+    return (
+        <div className="max-w-3xl space-x-4">
+            <h1>
+                Your Ideas, Documents & Plans. Unified. Welcome to Yotion
+            </h1>
+        </div>
+    )
+}
