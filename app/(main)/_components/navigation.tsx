4d1709ffa9e64cd07fa26027df00d7d89e9660d3
1davidyu1 <yudavid@msu.edu>
2024-06-01 18:23:12 -0500

    Trash Bin

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -17,6 +17,7 @@ import {
  } from "@/components/ui/popover"
 import { toast } from "sonner"
 import { DocumentList } from "./document-list"
+import { TrashBox } from "./trash-box"
 
 export const Navigation = () => {
     const pathname = usePathname()
@@ -167,7 +168,7 @@ export const Navigation = () => {
                       className="p-0 w-72"
                       side={isMobile ? "bottom" : "right"}
                     >
-                      <p>Trash box</p>
+                      <TrashBox />
                     </PopoverContent>
                   </Popover>
                 </div>
